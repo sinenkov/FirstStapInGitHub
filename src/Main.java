65dd25ac0b58a9f1d790ffc016be4ec8543ea14a
sinenkov <107352549+sinenkov@users.noreply.github.com>
2023-03-24 17:09:19 +0500

    Update Main.java
    1.3

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Отличная попытка!");
         System.out.println("Отличная попытка №2!");
+        System.out.println("Отличная попытка №3!");
 
     }
 }
